dd987d7d8a5447a0d62d6f02a98f883e35e2163a
Juan Trinidad <jtrinidad-luis@csumb.edu>
2021-02-09 22:17:46 -0800

    Lab 4

---
+++ lab4/js/lab.js
@@ -0,0 +1,8 @@
+// Creating a rock, paper, scissors game:
+  // 1.Create a start button, if user pushes start then game will begin
+  // 2.Set a adjusted time to allow users input.
+  // 3.Provide three options for user to pick.
+  // 4.Validate input
+  // 5.Once timer hits 0, play AI's answer.
+  // 6.AI's answer is generated randonmly based on the three available options.
+  // 7.Based on the users and AI's answer, the game will decide the winner.
