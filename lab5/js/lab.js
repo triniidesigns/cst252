138d72f3050959872ba3a30542c8ea7feed888e2
Juan Trinidad <jtrinidad-luis@csumb.edu>
2021-02-14 21:09:14 -0800

    Lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,22 @@
+/**
+ Author:    Juan Trinidad
+ Created:   04.14.2021
+ License:   Public Domain
+ **/
+
+// Declaring and Defining Variables
+make = "Honda";
+model = "Civic";
+color = "White";
+year = 2018;
+currentYear = 2021;
+
+var ownIt = true;
+var age = currentYear - year;
+
+// Print out
+document.write("Make: " + make + "<br>");
+document.write("Model: " + model + "<br>");
+document.write("Color: " + color + "<br>");
+document.write("Year: " + year + "<br>");
+document.write("Age: " + age + " years<br>");
