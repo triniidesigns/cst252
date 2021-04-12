48087cc3d69f6ed89934ffe4c6b6cbce74197dea
Juan Trinidad <triniidesigns@gmail.com>
2021-04-11 21:17:56 -0700

    Lab 17

---
+++ lab17/js/lab.js
@@ -0,0 +1,17 @@
+/**
+ Author:    Juan Trinidad
+ Created:   04.06.2021
+ License:   Public Domain
+ **/
+
+ $("#challenge-button").click(function(){
+ 	$("#first").toggleClass("one");
+ })
+
+ $("#problem-button").click(function(){
+ 	$("#second").toggleClass("two");
+ })
+
+ $("#results-button").click(function(){
+ 	$("#third").toggleClass("three");
+ })
