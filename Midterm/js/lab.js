678f3f747506cdf7367911e9e10e9500f83f37f2
Juan Trinidad <triniidesigns@gmail.com>
2021-04-18 23:36:47 -0700

    Midterm

--- Midterm/js/lab.js
+++ Midterm/js/lab.js
@@ -3,3 +3,16 @@
  Created:   04.17.2021
  License:   Public Domain
  **/
+
+$(document).ready(function(){
+// Init ScrollMagic
+var controller = new ScrollMagic.Controller();
+
+//build a scene
+var ourScene = new Scrollmagic.Scene({
+	triggerElement: '.tool'
+)}
+.setClassToggle('.tool', 'fade-in')
+.addIndicators()
+.addTo(controller);
+});
